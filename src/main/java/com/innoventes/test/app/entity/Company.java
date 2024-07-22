75a1f447a21b4de840bb1162797ce45acc5d81ae
abhijeettc <abhijeet09101997@gmail.com>
2024-07-22 16:05:58 +0530

    Task 1, Task 2

--- src/main/java/com/innoventes/test/app/entity/Company.java
+++ src/main/java/com/innoventes/test/app/entity/Company.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.*;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -30,15 +31,24 @@ public class Company extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "company_seq")
 	private Long id;
 
+	@NotBlank(message = "Can not be an empty string")
+	@Size(min=5, message = "Should be at least 5 characters of length")
 	@Column(name = "company_name")
 	private String companyName;
 
+	@NotBlank(message = "Mandatory")
+	@Email(message="Should be a valid email Id")
 	@Column(name = "email")
 	private String email;
-	
+
+	@PositiveOrZero
 	@Column(name = "strength")
 	private Integer strength;
 	
 	@Column(name = "website_url")
 	private String webSiteURL;
+
+	@Pattern(regexp = "^[a-zA-Z]}{2}[0-9]{2}[EN]?$", message = "Invalid company code format")
+	@Column(name = "company_code")
+	String companyCode;
 }
